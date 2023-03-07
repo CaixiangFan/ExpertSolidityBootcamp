e41c178e9bd15c245fc2837df3ab3b83fd0d371d
caixiangfan <caixiang@ualberta.ca>
2023-03-07 05:38:36 +0000

    solve assemb;y3

--- exercises/assembly/Assembly_3.sol
+++ exercises/assembly/Assembly_3.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
 
 contract SubOverflow {
     // Modify this function so that on overflow it returns the value 0
@@ -6,7 +7,12 @@ contract SubOverflow {
     function subtract(uint256 x, uint256 y) public pure returns (uint256) {
         // Write assembly code that handles overflows
         assembly {
-
+          let result := sub(x, y)
+          if lt(x, y) { result := 0 }
+          let ptr := mload(0x40)
+          let free_mem := add(ptr, 0x20)
+          mstore(free_mem, result)
+          return(free_mem, 0x20)
         }
     }
 }
